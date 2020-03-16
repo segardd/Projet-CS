6da6fac0d499536d74d3bfa258f3431860068afe
Donatien Segard <donatien.segard@gmail.com>
2020-03-16 09:42:06 +0100

    initial commit

---
+++ src/main/java/ProjetCS/ProjetCCS/App.java
@@ -0,0 +1,13 @@
+package ProjetCS.ProjetCCS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
